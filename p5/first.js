0a85f0ee000de23e9addc64666a5a9f9a83fb113
kushagragoenka <k.goenka9603@gmail.com>
2017-04-01 13:47:32 +0530

    Update first.js

--- p5/first.js
+++ p5/first.js
@@ -1,9 +1,23 @@
 function setup() { 
   createCanvas(400, 400);
-}
-
+} 
+var x = 50
+var y = 50
 function draw() { 
-   background(220);
-   ellipse(10, 10, 10);
+	background('cyan');
+  
+	
+  //arc(180,50,80,80,2,7.2,PIE);
+	if(x<90) {ellipse(100,50,15,15)};
+	
+	
+ if(x<200)
+	arc(x,50,80,80,.5,5.6,PIE);
+	if(x<130) {ellipse(140,50,15,15)}
+	
+	//{arc(x,50,80,80,.5,5.6,PIE);}
+	if(x<165) {ellipse(180,50,15,15)}
+	
+	//arc(x,y,80,.5,5.6,PIE)
 }
 
